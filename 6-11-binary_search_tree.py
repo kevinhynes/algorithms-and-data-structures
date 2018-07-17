f4a3aae0dbe3a4d7ed030e1a9ed23acf7bf1f5fc
kevin <khynes57@outlook.com>
2018-07-17 18:01:42 -0400

    added Node functions, BST __repr__

--- 6-11-binary_search_tree.py
+++ 6-11-binary_search_tree.py
@@ -5,6 +5,49 @@ class Node:
         self.right_child = None
         self.parent = None
 
+    def has_no_children(self):
+        if self.left_child == None and self.right_child == None:
+            return True
+
+    def has_one_child(self):
+        if self.left_child != None and self.right_child == None:
+            return True
+        elif self.left_child == None and self.right_child != None:
+            return True
+        else:
+            return False
+
+    def has_two_children(self):
+        if self.left_child != None and self.right_child != None:
+            return True
+        else:
+            return False
+
+    def has_left_child(self):
+        if self.left_child != None:
+            return True
+        else:
+            return False
+        
+    def has_right_child(self):
+        if self.right_child != None:
+            return True
+        else:
+            return False
+
+    def is_left_child(self):
+        if self.parent.left_child == self:
+            return True
+        else:
+            return False
+
+    def is_right_child(self):
+        if self.parent.right_child == self:
+            return True
+        else:
+            return False
+    
+
 class BST:
     def __init__(self):
         self.root = None
@@ -14,26 +57,27 @@ class BST:
         if self.root == None:
             return "There is no root to this tree."
         height = self.height()
-        width = height*9 + (2**height//2 - 1)
-        cur_height = 0
-        tree_list = [[] for i in range(height)]
-        #tree_list[cur_height].append(self.root)
-        tree_rep = self._repr(self.root, cur_height, tree_list)
-        tree_ret = ''
-        for row in tree_rep:
-            print(row)
-        tree_ret += ''.join(row) + '\n'
-        #print(tree_ret)
-        return tree_ret
+        leaf_nodes = 2**(height-1)      # Assume perfect binary tree to space nodes properly
+        width = (3*leaf_nodes*2) - 3    # 3 spaces per node & 3 spaces to separate nodes
+        cur_height, cur_index = 0, 0
+        tree_list = [['XXX' for j in range(2**i)] for i in range(height)]
+        tree_list = self._repr(self.root, cur_height, cur_index, tree_list, height)
+        tree_ascii = ''
+        for i in range(len(tree_list)):
+            row_as_string = ''
+            for n in tree_list[i]:
+                spacing = width//(len(tree_list[i])) + 1
+                row_as_string += n.center(spacing, ' ')
+            tree_ascii += row_as_string.center(width, ' ') + '\n'
+        return tree_ascii
 
-    def _repr(self, cur_node, cur_height, tree_list):
+    def _repr(self, cur_node, cur_height, cur_index, tree_list, height):
         if cur_node == None:
-            pass
+            return
         else:
-            tree_list[cur_height].append(str(cur_node.value))
-            tree_list[cur_height].append('   ')
-            self._repr(cur_node.left_child, cur_height + 1, tree_list)
-            self._repr(cur_node.right_child, cur_height + 1, tree_list)
+            tree_list[cur_height][cur_index] = str(cur_node.value)
+            self._repr(cur_node.left_child, cur_height + 1, cur_index*2, tree_list, height)
+            self._repr(cur_node.right_child, cur_height + 1, cur_index*2+1, tree_list, height)
         return tree_list
 
     def insert(self, value):
@@ -47,12 +91,14 @@ class BST:
         if value < cur_node.value:
             if cur_node.left_child == None:
                 cur_node.left_child = Node(value)
+                cur_node.left_child.parent = cur_node
                 self.size += 1
             else:
                 self._insert(value, cur_node.left_child)
         elif value > cur_node.value:
             if cur_node.right_child == None:
                 cur_node.right_child = Node(value)
+                cur_node.right_child.parent = cur_node
                 self.size += 1
             else:
                 self._insert(value, cur_node.right_child)
@@ -110,19 +156,93 @@ class BST:
             right_height = self._height(cur_node.right_child, cur_height + 1)
             return max(left_height, right_height)
 
+    def has_value(self, search_value):
+        if self.root == None:
+            return "There is no root to this tree."
+        else:
+            return self._search(search_value, self.root)
+            
+    def _has_value(self, search_value, cur_node):
+        if cur_node == None:
+            return False
+        elif search_value == cur_node.value:
+            return True
+        elif search_value < cur_node.value:
+            return self._search(search_value, cur_node.left_child)
+        elif search_value > cur_node.value:
+            return self._search(search_value, cur_node.right_child)
+        else:
+            return "An error occurred while searching."
+
     def search(self, search_value):
-        pass
-
-
+        if self.root == None:
+            return "There is no root to this tree."
+        else:
+            return self._search(search_value, self.root)
+            
     def _search(self, search_value, cur_node):
-        pass
+        if cur_node == None:
+            return None
+        elif search_value == cur_node.value:
+            return cur_node
+        elif search_value < cur_node.value:
+            return self._search(search_value, cur_node.left_child)
+        elif search_value > cur_node.value:
+            return self._search(search_value, cur_node.right_child)
+        else:
+            return "An error occurred while searching."
+    
+    def delete(self, node_val):
+        node_to_delete = self.search(node_val)
+        
+        if node_to_delete.has_no_children():
+            if node_to_delete.is_left_child():
+                node_to_delete.parent.left_child = None
+            elif node_to_delete.is_right_child():
+                node_to_delete.parent.right_child = None
 
+        elif node_to_delete.has_one_child():
+            if node_to_delete.is_left_child():    
+                if node_to_delete.has_left_child():
+                    node_to_delete.left_child.parent = node_to_delete.parent
+                    node_to_delete.parent.left_child = node_to_delete.left_child
+                elif node_to_delete.has_right_child():
+                    node_to_delete.right_child.parent = node_to_delete.parent
+                    node_to_delete.parent.right_child = node_to_delete.left_child
+            elif node_to_delete.is_right_child():
+                if node_to_delete.has_left_child():
+                    node_to_delete.left_child.parent = node_to_delete.parent
+                    node_to_delete.parent.right_child = node_to_delete.left_child
+                elif node_to_delete.has_right_child():
+                    node_to_delete.right_child.parent = node_to_delete.parent
+                    node_to_delete.parent.right_child = node_to_delete.right_child
+
+        elif node_to_delete.has_two_children():
+            node_list = []
+            successor = self.find_successor(node_to_delete, node_to_delete, node_list)
+            self.delete(successor)
+            node_to_delete.value = successor
+
+    def find_successor(self, node_to_delete, cur_node, node_list):
+        if cur_node != None:
+            self.find_successor(node_to_delete, cur_node.left_child, node_list)
+            if cur_node.value == node_to_delete.value:
+                return node_list[-1]
+            node_list.append(cur_node.value)
+            
+            self.find_successor(node_to_delete, cur_node.right_child, node_list)
+            if cur_node.value == node_to_delete.value:
+                return node_list[-1]
+            node_list.append(cur_node.value)
+            
+            
 
 tree = BST()
-tree.build_tree(15)
+tree.build_tree(11, 100)
 tree.print_tree()
 tree.print_tree('preorder')
 tree.print_tree('postorder')
-print(f'Tree height: {tree.height()}')
-print(tree.size)
+print(f'Tree height: {tree.height()}    Total nodes: {tree.size}')
+print(tree)
+tree.delete(9)
 print(tree)
